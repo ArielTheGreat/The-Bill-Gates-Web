26a9f4b5cdf2872d946dd173722484e43400b81d
Ariel Rocha <francoariel01@gmail.com>
2020-09-06 22:08:15 +0200

    Upload

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+
+function functionBiography(){
+    var x  = document.getElementById("mini-biography");
+    var y  = document.getElementById("historyMicrosoft");
+    var z = document.getElementById("youth-people");
+    y.style.display = "none";
+    z.style.display = "none";
+    if(x.style.display == "none")
+    {
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+}
+function history()
+{
+    var x  = document.getElementById("historyMicrosoft");
+    var y = document.getElementById("youth-people");
+    var z = document.getElementById("mini-biography");
+    y.style.display = "none";
+    z.style.display = "none";
+    if(x.style.display == "none")
+    {
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+}
+function youth()
+{
+    var x  = document.getElementById("youth-people");
+    var y  = document.getElementById("historyMicrosoft");
+    var z = document.getElementById("mini-biography");
+    y.style.display = "none";
+    z.style.display = "none";
+    if(x.style.display == "none")
+    {
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+}
